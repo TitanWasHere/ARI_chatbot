53aed53cbcef4bf2cae23a5df6e39b0b2b9a0817
TitanWasHere <andrea.gravili8@gmail.com>
2024-08-22 15:39:51 +0200

    adding history

--- chatbot/chatbot_devel/ui_chatbot.py
+++ chatbot/chatbot_devel/ui_chatbot.py
@@ -5,14 +5,17 @@ import dotenv
 import json
 import speech_recognition as sr
 from langchain_openai import AzureChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import JSONLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import create_retrieval_chain
-from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.memory import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+
 
 # Caricamento delle variabili d'ambiente
 dotenv.load_dotenv()
@@ -31,6 +34,88 @@ client = AzureOpenAI(
     azure_endpoint=azure_endpoint,
     api_key=api_key
 )
+
+def create_vector(docs):
+    embedding = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
+    vectorStore = FAISS.from_documents(docs, embedding=embedding)
+    return vectorStore
+
+def create_chain(vectorStore):
+    model = AzureChatOpenAI(
+        api_version=api_version,
+        azure_deployment=deployment_name,
+        azure_endpoint=azure_endpoint,
+        api_key=api_key
+    )
+
+    contextualize_q_system_prompt = """Data una cronologia della chat e l'ultima domanda dell'utente che potrebbe fare riferimento al contesto nella cronologia
+    della chat, formulare una domanda autonoma che possa essere compresa senza la cronologia della chat. 
+    NON rispondere alla domanda, riformulala se necessario e altrimenti restituiscila così com'è. 
+    """
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("history"),
+            ("human", "{input}")
+        ]
+    )
+    retriever = vectorStore.as_retriever(k=10)
+    
+    history_aware_retriever = create_history_aware_retriever(
+        model, retriever, contextualize_q_prompt
+    )
+
+    # Definizione del template del prompt
+    prompt = """
+    I valori di {topics} sono il nome della categoria con associata la descrizione e le parole chiave associate a quella categoria. 
+    I valori di {poi} sono i punti di interesse in cui vogliamo andare, 
+    i valori sono il nome del punto di ineteresse con associate le parole chiave di tale, il suo nome del file .wav associato e come viene chiamato. 
+    Quando l'utente ti fa una domanda, capisci a che topic si fa riferimento, se non si riferisce particolarmente a nessuna categoria, allora dai comunque una risposta. 
+    Se la categoria è \"goto\" allora dimmi il punto di interesse più simile associato altrimenti non dire nulla, per farlo dimmi il nome del punto di interesse dalla lista,
+    inoltre se non è esattamente chiaro a quale punto di interesse vuole andare, chiedi una conferma fra quelli disponibili usando il loro nome parlato
+    Se il punto di interesse viene confermato allora rispondi con "vado a 'nome punto di interesse'.
+
+    Contesto: {context} 
+    Domanda: {input}                                          
+    """
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt),
+            MessagesPlaceholder("history"),
+            ("human", "{input}")
+        ]
+    )
+    
+    #prompt.format(string=string)
+
+    chain = create_stuff_documents_chain(model, qa_prompt)
+
+    
+    retrieval_chain = create_retrieval_chain(
+        history_aware_retriever, chain
+    )
+
+    store = {}
+
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        retrieval_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="history",
+        output_messages_key="answer",
+    )
+
+
+
+    return conversational_rag_chain
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-4o"
@@ -76,55 +161,10 @@ data = data_topics + data_poi
 splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
 texts = splitter.split_documents(data)
 
-def create_vector(docs):
-    embedding = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-mpnet-base-v2"
-    )
-    vectorStore = FAISS.from_documents(docs, embedding=embedding)
-    return vectorStore
-
-def create_chain(vectorStore, topics, poi):
-    model = AzureChatOpenAI(
-        api_version=api_version,
-        azure_deployment=deployment_name,
-        azure_endpoint=azure_endpoint,
-        api_key=api_key
-    )
-
-    # Definizione del template del prompt
-    prompt = ChatPromptTemplate.from_template(
-    """
-    I valori di {topics} sono il nome della categoria con associata la descrizione e le parole chiave associate a quella categoria. 
-    I valori di {poi} sono i punti di interesse in cui vogliamo andare, 
-    i valori sono il nome del punto di ineteresse con associate le parole chiave di tale, il suo nome del file .wav associato e come viene chiamato. 
-    Quando l'utente ti fa una domanda, capisci a che topic si fa riferimento, se non è nessun topic allora rispondi generalmente e rispondi con il nome del topic. 
-    Se la categoria è \"goto\" allora dimmi il punto di interesse più simile associato altrimenti non dire nulla, per farlo dimmi il nome del punto di interesse dalla lista,
-    inoltre se non è esattamente chiaro a quale punto di interesse vuole andare, chiedi una conferma fra quelli disponibili usando il loro nome parlato
-    Se il punto di interesse viene confermato allora rispondi con "vado a 'nome punto di interesse'.
-
-    Contesto: {context} 
-    Domanda: {input}                                          
-    """)
-    
-    #prompt.format(string=string)
-
-    chain = create_stuff_documents_chain(
-        llm=model, 
-        prompt=prompt,
-    )
-
-    retriever = vectorStore.as_retriever(k=10)
-    retrieval_chain = create_retrieval_chain(
-        retriever,
-        chain
-    )
-
-    return retrieval_chain
-
 vectorStore = create_vector(texts)
 topics_str = json.dumps(topics)
 poi_str = json.dumps(poi)
-chain = create_chain(vectorStore, topics_str, poi_str)
+chain = create_chain(vectorStore)
 
 # Visualizzazione dei messaggi in Streamlit
 for message in st.session_state.messages:
@@ -177,11 +217,21 @@ if st.button("🎙️"):
 
         with st.chat_message("assistant"):
             # Utilizzo di chain.invoke per ottenere la risposta
-            response = chain.invoke({
-                "input": f"{speech_text}",
-                "poi": poi_str,
-                "topics": topics_str
-            })
+            # response = chain.invoke({
+            #     "input": f"{speech_text}",
+            #     "poi": poi_str,
+            #     "topics": topics_str
+            # })
+            response = chain.invoke(
+                {
+                    "input": f"{speech_text}",
+                    "poi": poi_str,
+                    "topics": topics_str
+                },
+                config={
+                    "configurable": {"session_id": "1234"}
+                }
+            )
             answer = response['answer']
 
             st.markdown(answer)
@@ -197,12 +247,22 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         # Utilizzo di chain.invoke per ottenere la risposta
-        response = chain.invoke({
-            "input": f"{prompt}",
-            "poi": poi_str,
-            "topics": topics_str
-            #string: string
-        })
+        # response = chain.invoke({
+        #     "input": f"{prompt}",
+        #     "poi": poi_str,
+        #     "topics": topics_str
+        #     #string: string
+        # })
+        response = chain.invoke(
+            {
+                "input": f"{prompt}",
+                "poi": poi_str,
+                "topics": topics_str
+            },
+            config={
+                "configurable": {"session_id": "1234"}
+            }
+        )
         answer = response['answer']
 
         st.markdown(answer)
