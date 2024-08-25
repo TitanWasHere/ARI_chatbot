3a052cc0f9ab55f8a0d2a2f5b9133aea5e6074bf
TitanWasHere <andrea.gravili8@gmail.com>
2024-08-26 00:01:47 +0200

    wavs

--- chatbot/ROS_chat/download_wavs.py
+++ chatbot/ROS_chat/download_wavs.py
@@ -18,23 +18,16 @@ class create_wavs:
 
         # Create a service server
         # String: fileName; txt
-        self.message = rospy.Service("wav_creator", String, self.callback)
+        self.sub = rospy.Subscriber("wav_creator", String, self.callback)
 
         rospy.spin()
 
     def callback(self, req):
-
-
-
         req = req.data.split(";")
         name = req[0]
         text = req[1]
-
         tts = gTTS(text, lang='it')    
-            
-
-
-
+        
         mp3name = "../wavs/" + wavs_name_dir + "/" + name + ".mp3"
         wavname = "../wavs/" + wavs_name_dir + "/" + name + ".wav"
         tts.save(mp3name)
